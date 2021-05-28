95ef5df8513f08feb4ad79509b73c93207dbd36e
unknown <josephsdo@gmail.com>
2021-05-28 18:23:16 +0200

    finish page fitness

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const toogleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toogleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');  
+}
